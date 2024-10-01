4a171f6ac98f224d79cfa63a8ba8156ab94f0c78
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-10-01 20:35:50 +0300

    Made a fix to my unnecessary longer steps code

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,25 +9,30 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int min, temp;
+	int min, location;
+	bool swap;
 
 	if (array == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
+		swap = false;
 		min = array[i];
 		for (j = i + 1; j < size; j++)
 		{
 			if (min > array[j])
 			{
-				temp = min;
 				min = array[j];
-				array[j] = temp;
+				location = j;
+				swap = true;
 			}
 		}
-		array[i] = min;
-		print_array(array, size);
-
+		if (swap == true)
+		{
+			array[location] = array[i];
+			array[i] = min;
+			print_array(array, size);
+		}
 	}
 }
